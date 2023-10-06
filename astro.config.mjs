d8cacfc594e4a579e9c3dbfe42f6db0e027006d8
nemanjam <nemanja.mitic.elfak@hotmail.com>
2023-10-06 20:02:17 +0200

    set domain and workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,8 +16,11 @@ import tailwind from "@astrojs/tailwind";
 const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://nemanjam.github.io";
+
+
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
