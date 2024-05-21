8b36a4e9310a58b589a4ab973538ea37e733adfe
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-05-21 19:27:17 +0200

    extract vars in bash script

--- scripts/deploy-nginx-local.sh
+++ scripts/deploy-nginx-local.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-# Navigate to the website folder on the remote server and clear contents of the website folder
-ssh arm1 'cd ~/traefik-proxy/apps/nmc-nginx-with-volume/website && \
-                        echo "Listing files before clearing:" && \
-                        echo "List before clearing:" && \
-                        ls && \
-                        echo "Count before clearing:" && \
-                        ls -l | grep -v ^l | wc -l && \
-                        echo "Clearing contents of the folder..." && \
-                        rm -rf * && \
-                        echo "List after clearing:" && \
-                        ls && \
-                        echo "Count after clearing:" && \
-                        ls -l | grep -v ^l | wc -l && \
-                        echo "Copying new contents..."'
+LOCAL_PATH="./dist"
+REMOTE_PATH="~/traefik-proxy/apps/nmc-nginx-with-volume/website"
+REMOTE_HOST="arm1"
 
-# Copy new contents
-scp -rq ./dist/* arm1:~/traefik-proxy/apps/nmc-nginx-with-volume/website
-# rsync -az --progress ./dist/* arm1:~/traefik-proxy/apps/nmc-nginx-with-volume/website
+# Navigate to the website folder on the remote server and clear contents of the website folder
+ssh $REMOTE_HOST "cd $REMOTE_PATH && \
+                  echo 'Listing files before clearing:' && \
+                  echo 'List before clearing:' && \
+                  ls && \
+                  echo 'Count before clearing:' && \
+                  ls -l | grep -v ^l | wc -l && \
+                  echo 'Clearing contents of the folder...' && \
+                  rm -rf * && \
+                  echo 'List after clearing:' && \
+                  ls && \
+                  echo 'Count after clearing:' && \
+                  ls -l | grep -v ^l | wc -l && \
+                  echo 'Copying new contents...'"
+
+# Copy new contents, 27MB
+# Using scp
+scp -rq $LOCAL_PATH/* $REMOTE_HOST:$REMOTE_PATH
+
+# Using rsync
+# rsync -az --progress $LOCAL_PATH/* $REMOTE_HOST:$REMOTE_PATH
+
+# Using tar and ssh cat
+# tar czf - -C $LOCAL_PATH . | ssh $REMOTE_HOST "cat > $REMOTE_PATH/dist.tar.gz && tar xzf $REMOTE_PATH/dist.tar.gz -C $REMOTE_PATH && rm $REMOTE_PATH/dist.tar.gz"
 
 
 # List all files after copying
-ssh arm1 'cd ~/traefik-proxy/apps/nmc-nginx-with-volume/website && \
-                        echo "List after copying:" && \
-                        ls && \
-                        echo "Count after copying:" && \
-                        ls -l | grep -v ^l | wc -l'
+ssh $REMOTE_HOST "cd $REMOTE_PATH && \
+                  echo 'List after copying:' && \
+                  ls && \
+                  echo 'Count after copying:' && \
+                  ls -l | grep -v ^l | wc -l"
+
 
 
 
