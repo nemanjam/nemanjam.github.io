39c6c02a3f440341f72a0d0678a1f9a5e03b54f2
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-05-21 14:57:48 +0200

    implement isActiveNavItem()

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,5 @@
+import { ROUTES } from '@/constants/routes';
+
 export const getRandomInt = (max: number, min = 0) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -12,4 +14,37 @@ export const trimHttpProtocol = (url: string) => {
   const withoutProtocol = withoutSlash.replace(protocolRegex, '');
 
   return withoutProtocol;
+};
+
+export const isActiveNavItem = ({
+  routePathname,
+  navItemHref,
+}: {
+  routePathname: string;
+  navItemHref: string;
+}): boolean => {
+  let isActive: boolean;
+
+  switch (true) {
+    // navItemHref identifies button, routePathname is route
+    case navItemHref === ROUTES.BLOG && routePathname === ROUTES.BLOG:
+    case navItemHref === ROUTES.ABOUT && routePathname === ROUTES.ABOUT:
+    case navItemHref === ROUTES.HOME && routePathname === ROUTES.HOME:
+    case navItemHref === ROUTES.RESUME && routePathname === ROUTES.RESUME:
+    case navItemHref === ROUTES.PROJECTS && routePathname.startsWith(ROUTES.PROJECTS):
+    case navItemHref === ROUTES.TAGS && routePathname.startsWith(ROUTES.TAGS):
+    case navItemHref === ROUTES.CATEGORIES && routePathname.startsWith(ROUTES.CATEGORIES):
+    case navItemHref === ROUTES.BLOG &&
+      !routePathname.startsWith(ROUTES.TAGS) &&
+      !routePathname.startsWith(ROUTES.CATEGORIES) &&
+      routePathname.startsWith(ROUTES.BLOG):
+      isActive = true;
+      break;
+
+    default:
+      isActive = false;
+      break;
+  }
+
+  return isActive;
 };
