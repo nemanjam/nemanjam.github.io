8dd615a093c9385b49f19c3c0eb1177f5d68aed0
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-05-26 10:29:43 +0200

    header name size

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type { ConfigType } from './types/config';
 const configData: ConfigType = {
   NODE_ENV: process.env.NODE_ENV,
   SITE_URL: process.env.SITE_URL,
-  SITE_TITLE: 'John Doe',
+  SITE_TITLE: 'Nemanja Mitic',
   SITE_DESCRIPTION: 'I am John Doe, eat at Joe',
   PAGE_SIZE: 3,
   MORE_POSTS_COUNT: 3,
