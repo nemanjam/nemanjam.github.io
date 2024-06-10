8e5cb6d586d946c013b62fb8d52c38e197a39764
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-06-10 13:00:07 +0200

    rename to getOpenGraphImagePath

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -8,7 +8,7 @@ const { OG_PAGES } = OG_IMAGE_PREFIXES;
 const homePageOgImage = `${ROUTES.API.OG_IMAGES}${OG_PAGES}.png`;
 const _404PageOgImage = `${ROUTES.API.OG_IMAGES}${OG_PAGES}404.png`;
 
-export const getDefaultOpenGraphImagePath = (path: string) => {
+export const getOpenGraphImagePath = (path: string) => {
   let trimmedPath = removeLeadingAndTrailingSlashes(path);
   let prefix = trimmedPath.split('/')[0];
 
