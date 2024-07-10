a4a9af904c21caa52f948e8844c8177ff84efb35
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-07-10 19:23:01 +0200

    add astro-embed and handle Youtube expand

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,2 +1,5 @@
-/** use only for components */
+/**
+ * Use only for components.
+ * Unused, use ComponentProps<typeof SomeComponent> instead.
+ */
 export type InferProps<T> = T extends (props: infer P) => JSX.Element ? P : never;
