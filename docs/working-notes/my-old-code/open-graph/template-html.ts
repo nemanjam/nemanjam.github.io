45efd8c79981a5504e10f500f07e5a011a0442ab
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-07-13 13:32:32 +0200

    add random color gradient

--- docs/working-notes/my-old-code/open-graph/template-html.ts
+++ docs/working-notes/my-old-code/open-graph/template-html.ts
@@ -1,7 +1,7 @@
 import { html } from 'satori-html';
+import colors from 'tailwindcss/colors';
 
-import { getRandomGradient } from '@/utils/gradients';
-import { limitString } from '@/utils/strings';
+import { getRandomElementFromArray, limitString } from '@/utils/strings';
 
 export interface TemplateProps {
   title: string;
@@ -9,6 +9,24 @@ export interface TemplateProps {
   avatarImageUrl: string;
   siteUrl: string;
 }
+
+const clrs = ['gray', 'indigo', 'yellow', 'blue', 'cyan', 'lime', 'sky'];
+const directions = ['to right', 'to bottom', '45deg'];
+const shades = [50, 100, 200];
+const rnd = getRandomElementFromArray;
+
+const getRandomGradient = () =>
+  `background: linear-gradient(${rnd(directions)}, ${colors[rnd(clrs)][rnd(shades)]}, ${colors[rnd(clrs)][rnd(shades)]})`;
+
+const gradients = [
+  `background: linear-gradient(to right, ${colors.gray[100]}, ${colors.gray[300]})`,
+  `background: linear-gradient(to right, ${colors.indigo[200]}, ${colors.yellow[100]})`,
+  `background: linear-gradient(to right, ${colors.blue[100]}, ${colors.blue[200]})`,
+  `background: linear-gradient(to right, ${colors.fuchsia[200]}, ${colors.cyan[200]})`,
+  `background: linear-gradient(to right, ${colors.teal[200]}, ${colors.lime[100]})`,
+  `background: linear-gradient(to right, ${colors.sky[200]}, ${colors.sky[100]})`,
+  `background: linear-gradient(to right, ${colors.sky[200]}, ${colors.cyan[100]})`,
+];
 
 const templateHtml = ({ title, heroImageUrl, avatarImageUrl, siteUrl }: TemplateProps) => {
   // 2 rows - max 30 chars
@@ -18,6 +36,7 @@ const templateHtml = ({ title, heroImageUrl, avatarImageUrl, siteUrl }: Template
   // max 6 rows x 10-15 chars
   const limitedTitle = limitString(title, 70);
 
+  // const randomGradient = getRandomElementFromArray(gradients);
   const randomGradient = getRandomGradient();
 
   return html`
@@ -35,7 +54,7 @@ const templateHtml = ({ title, heroImageUrl, avatarImageUrl, siteUrl }: Template
               alt="Nemanja Mitic"
               width="120"
               height="120"
-              class="rounded-full mr-8 border-2 border-gray-300"
+              class="rounded-full mr-8"
             />
             <div class="flex items-center ${isLongSiteUrl ? 'mt-4 text-3xl' : 'text-4xl'}">
               <div>${siteUrl}</div>
