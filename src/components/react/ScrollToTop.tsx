29fe94e79cf953a2941f5102da640601eba01db7
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-07-16 20:04:22 +0200

    move to Base layout, use position absolute, Explore list broken

--- src/components/react/ScrollToTop.tsx
+++ src/components/react/ScrollToTop.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-import { SELECTORS } from '@/constants/dom';
-
 import type { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
 }
-
-const { GISCUS_IFRAME_SELECTOR, GISCUS_WIDGET_SELECTOR } = SELECTORS;
 
 const fixedClasses = ['opacity-1', 'translate-y-0'];
 const hiddenClasses = ['opacity-0', 'translate-y-full'];
@@ -24,7 +20,6 @@ const hideLink = (linkRef: React.RefObject<HTMLAnchorElement>): void => {
 };
 
 const getHalfViewportHeight = (window: Window) => Math.floor(window.innerHeight / 2);
-const getDocumentScrollHeight = (document: Document) => document.documentElement.scrollHeight;
 
 const ScrollToTop: React.FC<Props> = ({ children }) => {
   const linkRef = useRef<HTMLAnchorElement>(null);
@@ -32,16 +27,15 @@ const ScrollToTop: React.FC<Props> = ({ children }) => {
   const bottomRef = useRef<HTMLDivElement>(null);
 
   const [height, setHeight] = useState(getHalfViewportHeight(window));
-  const [scrollHeight, setScrollHeight] = useState(getDocumentScrollHeight(document));
-
-  console.log('height', height, 'scrollHeight', scrollHeight);
 
   useEffect(() => {
     const callback: IntersectionObserverCallback = (entries) => {
-      const isAtTopOrBottom = entries.every((entry) => !entry.isIntersecting);
+      const isAtTopOrBottom = entries.some((entry) => entry.isIntersecting);
+
+      console.log('isAtTopOrBottom', isAtTopOrBottom);
 
       if (linkRef.current) {
-        isAtTopOrBottom ? showLink(linkRef) : hideLink(linkRef);
+        isAtTopOrBottom ? hideLink(linkRef) : showLink(linkRef);
       }
     };
 
@@ -52,31 +46,6 @@ const ScrollToTop: React.FC<Props> = ({ children }) => {
 
     return () => {
       intersect.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    const shadowHost = document.querySelector(GISCUS_WIDGET_SELECTOR);
-    const shadowRoot = shadowHost?.shadowRoot;
-    if (!shadowRoot) return;
-
-    const iframe = shadowRoot.querySelector(GISCUS_IFRAME_SELECTOR) as HTMLIFrameElement;
-
-    console.log('iframe', iframe);
-    if (!iframe) return;
-
-    console.log('updated scrollHeight', scrollHeight);
-
-    let timer: NodeJS.Timeout;
-    const handleLoad = () =>
-      (timer = setTimeout(() => setScrollHeight(getDocumentScrollHeight(document)), 3000));
-
-    // not onLoad but MutationObserver to lose class="loading"
-    iframe.addEventListener('load', handleLoad);
-
-    return () => {
-      clearTimeout(timer);
-      window.removeEventListener('load', handleLoad);
     };
   }, []);
 
@@ -102,8 +71,8 @@ const ScrollToTop: React.FC<Props> = ({ children }) => {
       />
       <div
         ref={bottomRef}
-        className="pointer-events-none absolute w-10 bg-blue-500"
-        style={{ height: `${height}px`, top: `${scrollHeight - height}px` }}
+        className="pointer-events-none absolute bottom-0 w-10 bg-blue-500"
+        style={{ height: `${height}px` }}
       />
       <a
         ref={linkRef}
