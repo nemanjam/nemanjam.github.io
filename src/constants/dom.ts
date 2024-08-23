d409fa55182cdb74ff6a2a38e7afcfa1230c6cd2
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-08-23 08:50:01 +0200

    update theme meta tag bg color

--- src/constants/dom.ts
+++ src/constants/dom.ts
@@ -6,4 +6,5 @@ export const SELECTORS = {
   GITHUB_MARKDOWN_BODY_SELECTOR: '#markdown-body-id',
   SCROLL_TO_TOP_ID: 'main-header',
   SCROLL_TO_TOP_SELECTOR: '#main-header',
+  THEME_META_TAG_SELECTOR: 'meta[name="theme-color"]',
 } as const;
