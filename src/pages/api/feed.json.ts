dadee5a2888c47d2b11191c6822e414a03d50876
Marc Andreu <marcandreuf@gmail.com>
2024-11-08 06:09:05 +0000

    clean up to minimise initial content

--- src/pages/api/feed.json.ts
+++
@@ -1,4 +0,0 @@
-import { feed } from '@/libs/api/feed';
-
-export const GET = () =>
-  new Response(feed.json1(), { status: 200, headers: { 'content-type': 'application/json' } });
--- src/pages/api/feed.xml.ts
+++
@@ -1,4 +0,0 @@
-import { feed } from '@/libs/api/feed';
-
-export const GET = () =>
-  new Response(feed.rss2(), { status: 200, headers: { 'content-type': 'application/rss+xml' } });
