9e68c7383ceb2ff641aa5da4f1db6c71ff71169f
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-04-24 11:55:46 +0200

    fix expressive code after rehype-external-links

--- plugins/rehype-external-links.ts
+++ plugins/rehype-external-links.ts
@@ -5,5 +5,11 @@ import type { Plugin } from 'unified';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const rehypeExternalLinks: [Plugin<any[], any>, any] = [
   rehypeExternalLinksPlugin,
-  { target: '_blank', rel: ['noopener', 'noreferrer'] },
+  {
+    target: '_blank',
+    rel: ['noopener', 'noreferrer'],
+    // filter out expressive-code, important
+    // skip <a> tags inside <pre> or <code>
+    selectors: 'a:not(pre a):not(code a)',
+  },
 ];
